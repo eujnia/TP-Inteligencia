86491cb53a10a1ed14c0f38e4f2049a6412724d2
Eugenia <19367235+eujnia@users.noreply.github.com>
2019-09-14 00:22:45 -0300

    update test capa gausiana

--- Guía 2/test_capa_gaussiana.m
+++ Guía 2/test_capa_gaussiana.m
@@ -3,13 +3,9 @@ file = 'XOR_trn.csv';
 entradas = csvread(file);  
 #defino los centroides iniciales
 
-[cg, yg] = capa_gaussiana(entradas(:,1:2), 2, 0.2); 
- 
- # muestro para cada par de entradas la salida estimada
- for i =1:size(entradas,1)
-   entradas(i,1:2)
-   cg(i)
- endfor
+[yg] = capa_gaussiana(entradas(:,1:2), 4, 0.2); 
+ entradas(1:10,1:2)
+ yg(1:10,:)
  
  
   
