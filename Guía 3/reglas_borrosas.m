10e80047473dd072b80725400e9a15ff874e7025
TiagoNLopez <tiago.n.lopez@gmail.com>
2019-10-30 13:26:59 -0300

    coso

--- Guía 3/reglas_borrosas.m
+++ Guía 3/reglas_borrosas.m
@@ -2,7 +2,8 @@ function [y]=reglas_borrosas(x,E,S,tipo,r)
   y = zeros(length(x),1);
   n = size(r,1);
   ga=zeros(size(x),size(E,1));
-  #x puede ser un valor o un vector e u g e n i a 
+  
+  
   for i=1:length(x)
     ga(i,:) = grado_activacion(E, tipo, x(i));
   endfor
