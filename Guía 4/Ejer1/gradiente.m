a21864028b11509733c1a76fd9bfa840b91dc909
Eugenia <19367235+eujenea@users.noreply.github.com>
2019-11-06 10:46:04 -0300

    Al fin anda bien el gradiente

--- Guía 4/Ejer1/gradiente.m
+++ Guía 4/Ejer1/gradiente.m
@@ -1,64 +1,90 @@
 function [mejor, lista_mejores, it] = gradiente(caso, x_ini_fin, y_ini_fin, criterio_corte, ventana, it_max)
     
-    % 0. Parámetros iniciales
+    % 0. Parametros iniciales
     switch caso
     case 1
       func = @(x) -x .* sin(sqrt(abs(x)));
-%      fitness = @(x) 450./((-x .* sin(sqrt(abs(x)))) + 450); 
-      fitness = @(x) exp(-(((x-424)/212).^2)); % gaussiana -> mejor convergencia
+%      error = @(x) 450./((-x .* sin(sqrt(abs(x)))) + 450); 
+      error = @(x) exp(-(((x-424)/212).^2)); % gaussiana -> mejor convergencia
       dfunc = @(x) -(x.^2*cos(sqrt(abs(x)))./(2*(abs(x)).^(3/2))) - sin(sqrt(abs(x)));
     case 2
       func = @(x) x + 5*sin(3*x) + 8*cos(5*x);
-%      fitness = @(x) 10./((x + 5*sin(3*x) + 8*cos(5*x)) + 10); 
-      fitness = @(x) exp(-(((x-1.8)/1.8).^2)); % gaussiana -> mejor convergencia
+%      error = @(x) 10./((x + 5*sin(3*x) + 8*cos(5*x)) + 10); 
+      error = @(x) exp(-(((x-1.8)/1.8).^2)); % gaussiana -> mejor convergencia
       dfunc = @(x) -40*sin(5*x) + 15*cos(3*x) + 1;
     case 3
       func = @(x,y) (x.^2 + y.^2).^(0.25) .* [(sin(50 .* (x.^2 + y.^2).^(0.1) )).^2 + 1];
-%      fitness = @(x,y) 1./(((x.^2 + y.^2).^(0.25) .* ((sin(50 .* (x.^2 + y.^2).^(0.1) )).^2 + 1)) + 1); 
-      fitness = @(x,y) exp(-(((x/50).^2)+((y/50).^2))); % gaussiana -> mejor convergencia
+%      error = @(x,y) 1./(((x.^2 + y.^2).^(0.25) .* ((sin(50 .* (x.^2 + y.^2).^(0.1) )).^2 + 1)) + 1); 
+      error = @(x,y) exp(-(((x/50).^2)+((y/50).^2))); % gaussiana -> mejor convergencia
       dx = @(x,y) .5.*x.*(sin(50.*(x.^2+y.^2).^(.1)).^2+1)./(x.^2+y.^2).^(.75) + 12.*x.*(100.*(x.^2+y.^2).^(.1))./(x.^2+y.^2).^(.65);
       dy = @(x,y) .5.*y.*(sin(50.*(x.^2+y.^2).^(.1)).^2+1)./(x.^2+y.^2).^(.75) + 12.*y.*(100.*(x.^2+y.^2).^(.1))./(x.^2+y.^2).^(.65);
     endswitch
     lista_mejores = [];
     
-    if caso != 3
-      mejor = rand();
+    # inicializamos el x y el mejor con rand
+    if caso != 3 # caso 1D
+      mejor = [x_ini_fin(1) + (x_ini_fin(2) - x_ini_fin(1)) * rand()];
       x = [x_ini_fin(1) + (x_ini_fin(2) - x_ini_fin(1)) * rand()];
     else
-      mejor = rand(2,1);
+      mejor = [x_ini_fin(1) + (x_ini_fin(2) - x_ini_fin(1)) *rand(),
+               y_ini_fin(1) + (y_ini_fin(2) - y_ini_fin(1)) *rand()];
+           
       x = [x_ini_fin(1) + (x_ini_fin(2) - x_ini_fin(1)) *rand(),
            y_ini_fin(1) + (y_ini_fin(2) - y_ini_fin(1)) *rand()];
     endif
     
     figure;
-    if caso != 3
+    if caso != 3 # caso 1D
       x_plot = linspace( x_ini_fin(1), x_ini_fin(2), 1000 );
       plot( x_plot, func(x_plot), 'k', 'linewidth', 1.1); hold on;
-    else
+    else # caso 2D
       x_plot = linspace( x_ini_fin(1), x_ini_fin(2), 1000 );
       y_plot = linspace( y_ini_fin(1), y_ini_fin(2), 1000 );
+      
       [coordx, coordy] = meshgrid(x_plot, y_plot);
       contour(coordx, coordy, func(coordx, coordy), 5); hold on;
+      pause(.5);
     endif
     
     for i=1:it_max
       
-      if caso != 3
+      # gradiente 
+      if caso != 3 
         grad_x = dfunc(x);
       else
         grad_x = [dx(x(1),x(2)), dy(x(1),x(2))];
       endif
-      x = x - grad_x;
-      lista_mejores = [lista_mejores; x];
+      
+      alpha = actualizar_alpha(x,grad_x,caso);
+      x_aux = x - grad_x.*alpha;
+      
       if caso != 3
-        if i>1 && fitness(x(1)) > fitness(lista_mejores(i-1,1))
+        if x_aux >= x_ini_fin(1) && x_aux <= x_ini_fin(2) 
+          x=x_aux;
+        endif        
+        if i>1 && error(x(1)) > error(lista_mejores(i-1,1))
           mejor = x;
         endif
       else
-        if i>1 && fitness(x(1),x(2)) > fitness(lista_mejores(i-1,1),lista_mejores(i-1,2))
+        
+        if x_aux(1) >= x_ini_fin(1) && x_aux(1) <= x_ini_fin(2) &&  x_aux(2) >= y_ini_fin(1) && x_aux(2) <= y_ini_fin(2) 
+            x=x_aux;
+          
+        else 
+          if x_aux(1) < x_ini_fin(1)
+            x=[x_ini_fin(1), y_ini_fin(1)];
+          else
+            x=[x_ini_fin(2), y_ini_fin(2)];
+          endif 
+        endif
+        
+       
+        if i>1 && error(x(1),x(2)) > error(lista_mejores(i-1,1),lista_mejores(i-1,2))
           mejor = x;
         endif
       endif
+      
+      lista_mejores = [lista_mejores; x];
       
       if caso != 3
         hold off;
@@ -78,7 +104,7 @@ function [mejor, lista_mejores, it] = gradiente(caso, x_ini_fin, y_ini_fin, crit
       endif
       
       if i == it_max
-        mejor = lista_mejores(it_max);
+        mejor = lista_mejores(it_max,:);
       endif
       
     endfor
--- Guía 4/Ejer1/test_grad.m
+++ Guía 4/Ejer1/test_grad.m
@@ -1,7 +1,7 @@
 criterio_corte = 0.1;
 ventana = 10;
 it_max = 100;
-
+y_ini_fin = [];
 for i=1:3
   switch i
     case 1
@@ -9,10 +9,10 @@ for i=1:3
     case 2
       x_ini_fin = [0, 20];
     case 3
-      x_ini_fin = [-100, 100]; 
+      x_ini_fin = [-100, 100];
       y_ini_fin = [-100, 100];
   endswitch
   
-  [mejor, lista_mejores, it] = gradiente(i, x_ini_fin, y_ini_fin, criterio_corte, ventana, it_max)
+  [mejor, lista_mejores, it] = gradiente(i, x_ini_fin,y_ini_fin, criterio_corte, ventana, it_max)
     
 endfor
